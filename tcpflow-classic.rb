96a24d5fcd0428b84b8250b5cb8f7e6dba1d230f
Richard Friend <rik.friend@gmail.com>
2018-07-12 17:03:31 +1000

    sha1 -> sha256

--- tcpflow-classic.rb
+++ tcpflow-classic.rb
@@ -3,7 +3,7 @@ require "formula"
 class TcpflowClassic < Formula
   homepage "http://www.circlemud.org/jelson/software/tcpflow/"
   url "https://github.com/rikf/tcpflow-classic/archive/v0.22-osx.tar.gz"
-  sha1 "1eccd1074141a7ef165e885cc69cdc23e55b4aa1"
+  sha256 "3faf35c7a01811c9b159742c9ab42e4ad54207989105c525547cf56b02185503"
   def install
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
